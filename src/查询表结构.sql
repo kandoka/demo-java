136026bb1a25eabc2468b05aade90f1b4de65263
dohan <dohan@student.unimelb.edu.au>
2021-01-23 16:01:35 +0800

    sql获取表结构，解析html

--- src/查询表结构.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `查询表结构` VALUES ('id', 'int(11)', 'int', NULL, 'NO', NULL, '商品id,主键自增');
-INSERT INTO `查询表结构` VALUES ('goods_name', 'varchar(20)', 'varchar', 20, 'YES', NULL, '商品名');
-INSERT INTO `查询表结构` VALUES ('goods_sort', 'int(11)', 'int', NULL, 'NO', NULL, '商品种类');
-INSERT INTO `查询表结构` VALUES ('goods_price', 'double', 'double', NULL, 'YES', NULL, '商品价格');
-INSERT INTO `查询表结构` VALUES ('goods_band', 'int(11)', 'int', NULL, 'YES', NULL, '商品品牌');
